49f6bfead1939c7226276848512a03263c5aec2c
Ariel <84594293+larielol@users.noreply.github.com>
2024-11-18 11:24:15 -0300

    Add files via upload

---
+++ Laboratórios/Lab03/LAB3/Lab3LUCASARIEL/src/agenda/Agenda.java
@@ -0,0 +1,55 @@
+package agenda;
+
+/**
+ * Uma agenda que mantém uma lista de contatos com posições. Podem existir 100 contatos. 
+ * 
+ * @author nazareno
+ *
+ */
+public class Agenda {
+	
+	private static final int TAMANHO_AGENDA = 100;
+	
+	private Contato[] contatos; //apenas uma simplificacao de contato
+
+	/**
+	 * Cria uma agenda.
+	 */
+	public Agenda() {
+		this.contatos = new Contato[TAMANHO_AGENDA];
+	}
+	
+	/**
+	 * Acessa a lista de contatos mantida.
+	 * @return O array de contatos.
+	 */
+	public String[] getContatos() {
+		return this.contatos.clone();
+	}
+
+	/**
+	 * Acessa os dados de um contato específico.
+	 * @param posicao Posição do contato na agenda.
+	 * @return Dados do contato. Null se não há contato na posição.
+	 */
+	public String getContato(int posicao) {
+		return contatos[posicao];
+	}
+
+	/**
+	 * Cadastra um contato em uma posição. Um cadastro em uma posição que já existe sobrescreve o anterior. 
+	 * @param posicao Posição do contato.
+	 * @param nome Nome do contato.
+	 * @param sobrenome Sobrenome do contato.
+	 * @param telefone Telefone do contato.
+	 */
+	
+	public boolean cadastraContato(int posicao, String nome, String sobrenome, String telefone) {
+		contatoAux = new Contato(nome, sobrenome, telefone);
+		for contato: c
+	}
+	
+	public void cadastraContato(int posicao, String nome, String sobrenome, String telefone) {
+		this.contatos[posicao - 1] = new Contato(nome, sobrenome, telefone);
+		}
+	}
---
+++ Laboratórios/Lab03/LAB3/Lab3LUCASARIEL/src/agenda/Contato.java
@@ -0,0 +1,30 @@
+package agenda;
+
+import java.util.Objects;
+
+public class Contato {
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+	
+	public Contato(String nome, String sobrenome, String telefone) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.telefone = telefone;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(nome, other.nome) && Objects.equals(sobrenome, other.sobrenome);
+	}
+	
+	
+	
+}
